976fdc9806038511c8643ba25f2d7b41e0473f70
Rahmatia21 <147253827+Rahmatia21@users.noreply.github.com>
2023-10-13 17:01:52 +0800

    Add files via upload

---
+++ Saya.java
@@ -0,0 +1,13 @@
+
+package saya;
+
+public class Saya {
+
+    public static void main(String[] args) {
+     int angka = 10;
+        System.out.println("nilai awal anda: " + angka);
+        angka = 10;
+        System.out.println("nilai setelah perulagan: " + angka);
+    }
+    
+}
