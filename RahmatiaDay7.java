124bda5a20b59d997249ef5cac5652c8372c541e
Rahmatia21 <147253827+Rahmatia21@users.noreply.github.com>
2023-10-15 04:22:44 -0700

    Add files via upload

---
+++ RahmatiaDay7.java
@@ -0,0 +1,14 @@
+
+package rahmatia.day7;
+
+
+public class RahmatiaDay7 {
+
+    public static void main(String[] args) {
+        for (int i = 1; i < 100; i++){
+            System.out.println("infotmatika");
+        }
+      
+    }
+    
+}
