853190c0849327b7941ff900ed1a0bd4b450867b
Rahmatia21 <147253827+Rahmatia21@users.noreply.github.com>
2023-10-16 04:27:36 -0700

    Add files via upload

---
+++ HariKe_8.java
@@ -0,0 +1,18 @@
+
+package hari.ke_8;
+
+public class HariKe_8 
+{
+   
+    public static void main(String[] args) {
+       int a,b,c,saya;
+        
+        a = 20;
+        b = 10;
+        c = 3; 
+        saya = a*b+c;
+        
+        System.out.println("munculkan nilai saya:"+saya);   
+    }
+    
+}
