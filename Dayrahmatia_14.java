abc05141b25b2a8003c78b48e2e4af553b6fc965
Rahmatia21 <147253827+Rahmatia21@users.noreply.github.com>
2023-10-22 17:20:26 +0700

    Add files via upload

---
+++ Dayrahmatia_14.java
@@ -0,0 +1,18 @@
+
+
+package com.mycompany.dayrahmatia_14;
+
+
+public class Dayrahmatia_14 {
+
+    public static void main(String[] args) {
+    int a = 10;
+       for (int b = 1; b <= a; b++){
+           for (int c = 1; c <= b; c++) {
+               System.out.print('*');
+           }
+           System.out.println();
+       }
+    }
+}
+
