c36ba33e0d177290110ee6c2b01779c402e0c18e
Rahmatia21 <147253827+Rahmatia21@users.noreply.github.com>
2023-11-02 23:36:42 +0800

    Add files via upload

---
+++ Day25_rahmatia.java
@@ -0,0 +1,18 @@
+
+package com.mycompany.day25_rahmatia;
+
+
+public class Day25_rahmatia {
+
+    public static void main(String[] args) {
+       int pembelian = 110;
+        System.out.println("Jumlah pembelian = "+pembelian);
+       
+        if (pembelian>=100){
+            System.out.println("Diskon = 20% ");
+        }
+        else{
+            System.out.println("Diskon 5%");
+        }
+    }
+}
