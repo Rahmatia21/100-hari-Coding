08e18b95e89a67abc9e9ebeee7470e4d444c3abc
Rahmatia21 <147253827+Rahmatia21@users.noreply.github.com>
2023-11-06 21:24:50 +0700

    Add files via upload

---
+++ Day29_rahmatia.java
@@ -0,0 +1,12 @@
+
+package com.mycompany.day29_rahmatia;
+
+public class Day29_rahmatia {
+
+    public static void main(String[] args) {
+        String nim, nama, jurusan, fakultas;
+        int jumlah =9;
+        for(int i=0;i<9;i++)
+        System.out.println("jumlah");  
+    }
+}
