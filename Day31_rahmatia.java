564999b9df869d5e82ed62f6cb0eb2856fdef546
Rahmatia21 <147253827+Rahmatia21@users.noreply.github.com>
2023-11-08 19:15:12 +0700

    Add files via upload

---
+++ Day31_rahmatia.java
@@ -0,0 +1,15 @@
+
+
+package com.mycompany.day31_rahmatia;
+
+public class Day31_rahmatia {
+
+    public static void main(String[] args) {
+        int []numbers = {8,1,7,2,10,6,4,3,9,5};
+        int i = 0;
+        do {
+            System.out.println("noumber:"+ numbers[i]);
+            i++;
+        }while (i < numbers.length);  
+    }
+}
