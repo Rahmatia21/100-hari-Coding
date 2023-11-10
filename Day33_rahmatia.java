9d1041a81b1c3a58cf7a61967a00c3ae28dedb11
Rahmatia21 <147253827+Rahmatia21@users.noreply.github.com>
2023-11-10 17:56:57 +0700

    Add files via upload

---
+++ Day33_rahmatia.java
@@ -0,0 +1,18 @@
+
+package day33_rahmatia;
+
+
+public class Day33_rahmatia {
+
+    
+    public static void main(String[] args) {
+    for (int i=1;i<10;i++){
+        if (i % 2 == 0) { // akan bernilai true bila i genap
+            System.out.println(i+" adalah bilangan genap");
+        } else {
+            System.out.println(i+" adalah bilangan ganjil");
+        }
+    }
+    }
+    
+}
