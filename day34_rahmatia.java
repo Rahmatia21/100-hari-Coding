c8e0aed5beaee4900e3bc6e745b2e5d0bafc3546
Rahmatia21 <147253827+Rahmatia21@users.noreply.github.com>
2023-11-11 18:46:30 +0700

    Add files via upload

---
+++ day34_rahmatia.java
@@ -0,0 +1,16 @@
+
+package isc;
+
+import java.util.Scanner;
+
+
+public class day34_rahmatia {
+    public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Masukkan nama Anda: ");
+        String nama = scanner.nextLine();
+
+        System.out.println("Selamat datang, " + nama + "!");   
+    }
+}
