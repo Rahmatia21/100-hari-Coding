e2d4b5914462da826962d87bc318a31a147be718
Rahmatia21 <147253827+Rahmatia21@users.noreply.github.com>
2023-11-12 19:56:21 +0700

    Add files via upload

---
+++ day35_rahmatia.java
@@ -0,0 +1,24 @@
+
+package isc;
+
+import java.util.LinkedList;
+
+
+public class day35_rahmatia {
+    public static void main(String[] args) {
+        
+        LinkedList<String> list = new LinkedList<>();
+
+        
+        list.add("A");
+        list.add("B");
+        list.add("C");
+
+       
+        for (int i = list.size() - 1; i >= 0; i--) {
+            System.out.println(list.get(i));
+        }
+    
+    }
+  
+}
