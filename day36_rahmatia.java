ca2f78fb3b6941845c038bdd47a608a3e1ce561f
Rahmatia21 <147253827+Rahmatia21@users.noreply.github.com>
2023-11-12 19:46:24 +0700

    Add files via upload

---
+++ day36_rahmatia.java
@@ -0,0 +1,17 @@
+package isc;
+
+
+public class day36_rahmatia {
+    public static void main(String[] args) {
+      // Inisialisasi variabel double
+        double i = 1.0;
+        double j = 10.0;
+
+        // Perulangan
+        while (i <= j) {
+
+            System.out.println(i);
+            i += 1.0;
+        }
+    }
+}
